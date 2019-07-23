f7ebbe196eccae3dec4bee4afa2f76dbaa25218a
eliagitai <elia.ahadi@gmail.com>
2019-07-23 13:00:58 +0900

    added cors to rails config and cors

--- config.ru
+++ config.ru
@@ -2,4 +2,12 @@
 
 require_relative 'config/environment'
 
+require 'rack/cors'
+
+use Rack::Cors do
+  allow do
+    origins '*'
+    resource '*', headers: :any, methods: [:get, :post, :options, :put]
+  end
+end
 run Rails.application
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -15,9 +15,19 @@
 #   end
 # end
 
+# Rails.application.config.middleware.insert_before 0, Rack::Cors do
+#   allow do
+#     origins 'localhost:4000'
+#     resource '*',
+#       headers: :any,
+#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
+#   end
+# end
+
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:4000'
+    origins '*'
+
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
